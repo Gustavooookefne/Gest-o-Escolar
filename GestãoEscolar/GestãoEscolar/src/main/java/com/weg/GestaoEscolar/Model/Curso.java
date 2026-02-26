e0972044996523ef36b7bf48db916ae11d8a289a
Gustavooookefne <gustavooookefne14@gmail.com>
2026-02-26 15:14:49 -0300

    feat: Finalizado o Curso Model

--- GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Model/Curso.java
+++ GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Model/Curso.java
@@ -1,4 +1,42 @@
 package com.weg.GestaoEscolar.Model;
 
 public class Curso {
+    private int id;
+    private String nomeCurso;
+    private int codigo;
+
+    public Curso(int id, String nomeCurso, int codigo) {
+        this.id = id;
+        this.nomeCurso = nomeCurso;
+        this.codigo = codigo;
+    }
+
+    public Curso(String nomeCurso, int codigo) {
+        this.nomeCurso = nomeCurso;
+        this.codigo = codigo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 }
