f981606806f80e4f04e53906d54809ff2ea9c8b9
Gustavooookefne <gustavooookefne14@gmail.com>
2026-02-26 15:28:33 -0300

    feat: Finalizad o Model Turma

--- GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Model/Turma.java
+++ GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Model/Turma.java
@@ -1,4 +1,53 @@
 package com.weg.GestaoEscolar.Model;
 
 public class Turma {
+    private int id;
+    private String nomeTurma;
+    private Curso curso;
+    private Professor professor;
+
+    public Turma(int id, String nomeTurma, Curso curso, Professor professor) {
+        this.id = id;
+        this.nomeTurma = nomeTurma;
+        this.curso = curso;
+        this.professor = professor;
+    }
+
+    public Turma(String nomeTurma, Curso curso, Professor professor) {
+        this.nomeTurma = nomeTurma;
+        this.curso = curso;
+        this.professor = professor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeTurma() {
+        return nomeTurma;
+    }
+
+    public void setNomeTurma(String nomeTurma) {
+        this.nomeTurma = nomeTurma;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
 }
