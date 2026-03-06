9a7e04d4ee34f2aa1a43ce84df2c311554113053
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-06 16:27:21 -0300

    feat: Finalizado o CursoMapper

--- GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/CursoMapper.java
+++ GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/CursoMapper.java
@@ -1,4 +1,31 @@
 package com.weg.GestaoEscolar.Mapper;
 
+import com.weg.GestaoEscolar.Dtos.CursoDtos.CursoRequestDtos;
+import com.weg.GestaoEscolar.Dtos.CursoDtos.CursoResponseDtos;
+import com.weg.GestaoEscolar.Model.Curso;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CursoMapper {
+
+    public Curso paraEntidade(CursoRequestDtos cursoRequestDtos){
+
+        return new Curso(
+
+                cursoRequestDtos.nomeCurso(),
+                cursoRequestDtos.codigo()
+        );
+
+    }
+
+    public CursoResponseDtos paraDtos (Curso curso){
+
+        return new CursoResponseDtos(
+
+                curso.getId(),
+                curso.getNomeCurso(),
+                curso.getCodigo()
+        );
+
+    }
 }
