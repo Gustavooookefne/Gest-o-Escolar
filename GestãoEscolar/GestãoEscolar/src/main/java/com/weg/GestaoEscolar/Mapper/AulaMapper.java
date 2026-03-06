791b50ea881fb639d27c28a09ccecceb156d0ab4
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-06 16:25:15 -0300

    feat: Finalizado o AulaMapper

--- GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/AulaMapper.java
+++ GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/AulaMapper.java
@@ -1,4 +1,33 @@
 package com.weg.GestaoEscolar.Mapper;
 
+import com.weg.GestaoEscolar.Dtos.AulaDtos.AulaRequestDtos;
+import com.weg.GestaoEscolar.Dtos.AulaDtos.AulaResponseDtos;
+import com.weg.GestaoEscolar.Model.Aula;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AulaMapper {
+
+    public Aula paraEntidades(AulaRequestDtos aulaRequestDtos){
+
+        return new Aula(
+
+                aulaRequestDtos.turma(),
+                aulaRequestDtos.dataHora(),
+                aulaRequestDtos.assuntos()
+        );
+
+    }
+
+    public AulaResponseDtos paraDtos(Aula aula){
+
+        return new AulaResponseDtos(
+
+                aula.getId(),
+                aula.getTurma(),
+                aula.getDataHora(),
+                aula.getAssuntos()
+        );
+    }
+
 }
