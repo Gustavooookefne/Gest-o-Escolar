e33fcfd8baf424549bf8a29fc453e30afba16c4a
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-06 16:32:28 -0300

    feat: Finalizado o ProfessorMapper

--- GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/ProfessorMapper.java
+++ GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/ProfessorMapper.java
@@ -1,4 +1,32 @@
 package com.weg.GestaoEscolar.Mapper;
 
+import com.weg.GestaoEscolar.Dtos.ProfessorDtos.ProfessorRequestDtos;
+import com.weg.GestaoEscolar.Dtos.ProfessorDtos.ProfessorResponseDtos;
+import com.weg.GestaoEscolar.Model.Professor;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProfessorMapper {
+
+    public Professor paraEntidade(ProfessorRequestDtos professorRequestDtos){
+
+        return new Professor(
+
+                professorRequestDtos.nome(),
+                professorRequestDtos.email(),
+                professorRequestDtos.disiplina()
+
+        );
+    }
+
+    public ProfessorResponseDtos paraDtos(Professor professor){
+
+        return new ProfessorResponseDtos(
+                professor.getId(),
+                professor.getNome(),
+                professor.getEmail(),
+                professor.getDisiplina()
+
+        );
+    }
 }
