33885fcc709714ac9d831107446f0964be6cefbf
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-06 16:34:27 -0300

    feat: Finalizado o TurmaMapper

--- GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/TurmaMapper.java
+++ GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/TurmaMapper.java
@@ -1,4 +1,30 @@
 package com.weg.GestaoEscolar.Mapper;
 
+import com.weg.GestaoEscolar.Dtos.TurmaDtos.TurmaRequestDtos;
+import com.weg.GestaoEscolar.Dtos.TurmaDtos.TurmaResponseDtos;
+import com.weg.GestaoEscolar.Model.Turma;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TurmaMapper {
+
+    public Turma paraEntidade (TurmaRequestDtos turmaRequestDtos){
+
+        return new Turma(
+
+                turmaRequestDtos.nomeTurma(),
+                turmaRequestDtos.curso(),
+                turmaRequestDtos.professor()
+        );
+    }
+
+    public TurmaResponseDtos paraDtos(Turma turma){
+
+        return new TurmaResponseDtos(
+                turma.getId(),
+                turma.getNomeTurma(),
+                turma.getCurso(),
+                turma.getProfessor()
+        );
+    }
 }
