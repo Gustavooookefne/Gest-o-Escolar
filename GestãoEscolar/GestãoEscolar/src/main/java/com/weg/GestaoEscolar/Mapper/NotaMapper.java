3b4e73e87b750282e1b4df70f8d12a034ba0a390
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-06 16:29:33 -0300

    feat: Finalizado o NotaMapper

--- GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/NotaMapper.java
+++ GestãoEscolar/GestãoEscolar/src/main/java/com/weg/GestaoEscolar/Mapper/NotaMapper.java
@@ -1,4 +1,32 @@
 package com.weg.GestaoEscolar.Mapper;
 
+import com.weg.GestaoEscolar.Dtos.NotaDtos.NotaRequestDtos;
+import com.weg.GestaoEscolar.Dtos.NotaDtos.NotaResponseDtos;
+import com.weg.GestaoEscolar.Model.Nota;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NotaMapper {
+
+    public Nota paraEntidade(NotaRequestDtos notaRequestDtos){
+
+        return new Nota(
+
+                notaRequestDtos.aluno(),
+                notaRequestDtos.aula(),
+                notaRequestDtos.nota()
+        );
+
+    }
+
+    public NotaResponseDtos paraDtos(Nota nota){
+
+        return new NotaResponseDtos(
+
+                nota.getId(),
+                nota.getAluno(),
+                nota.getAula(),
+                nota.getNota()
+        );
+    }
 }
